b4c71c8238b9023f2c634b15567ddaca0ebdd7c3
mynewwfaccount2017 <mynewwfaccount2017@mail.ru>
2022-05-17 13:00:50 +0300

    2

--- src/Program.cs
+++ src/Program.cs
@@ -10,6 +10,11 @@ namespace src
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Введите количество продуктов:");
+
+            int productsMANY = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
